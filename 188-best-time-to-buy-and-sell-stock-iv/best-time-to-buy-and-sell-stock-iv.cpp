290239c6e504e2299de2ad8704829ce2ed9f3587
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2025-03-27 10:59:43 +0530

    Time: 289 ms (5.32%) | Memory: 49.8 MB (5.90%) - LeetSync

---
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    map<pair<int,pair<int,bool>>,int> dp;
+    int getans(int k, vector<int>& prices,bool buyed,int i=0){
+        if(i>=prices.size() or k<=0){
+            return 0;
+        }
+
+        if(dp.find({i,{k,buyed}})!=dp.end()){
+            return dp[{i,{k,buyed}}];
+        }
+
+        int w1=INT_MIN,w2=INT_MIN,w3=INT_MIN;
+        // buy
+        if(!buyed){
+            w1=max(w1,-prices[i]+getans(k,prices,true,i+1));
+        }
+
+        // sell
+        if(buyed){
+            w2=max(w1,prices[i]+getans(k-1,prices,false,i+1));
+        }
+
+        // skip
+        w3=max(w3,getans(k,prices,buyed,i+1));
+
+        return dp[{i,{k,buyed}}]=max({w1,w2,w3});
+    }
+    int maxProfit(int k, vector<int>& prices) {
+        bool buyed=false;
+        return getans(k,prices,buyed,0);
+    }
+};
+
+/*
+
+
+*/
