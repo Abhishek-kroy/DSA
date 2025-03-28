1583c2c27482d85aa503b68ca629b3770cba94e4
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2025-03-28 13:20:19 +0530

    Time: 0 ms (100.00%) | Memory: 15.9 MB (5.13%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        stack<pair<int,int>> st;
+        // stack<pair<int,int>> st;
         int n=nums.size();
-        st.push({nums.back(),n-1});
+        int st=n-1;
+        // st.push({nums.back(),n-1});
         for(int i=n-2;i>=0;i--){
-            if(nums[i]>=st.top().first){
-                st.push({nums[i],i});
+            // if(nums[i]>=st.top().first){
+            //     st.push({nums[i],i});
+            // }
+            if(nums[i]>=nums[st]){
+                // st.push({nums[i],i});
+                st--;
             }
+            // else{
+            //     int index;
+            //     while(!st.empty() && st.top().first>nums[i]){
+            //         index=st.top().second;
+            //         st.pop();
+            //     }
+            //     swap(nums[i],nums[index]);
+            //     sort(nums.begin()+i+1,nums.end());
+            //     return;
+            // }
             else{
                 int index;
-                while(!st.empty() && st.top().first>nums[i]){
-                    index=st.top().second;
-                    st.pop();
+                while(st<n && nums[st]>nums[i]){
+                    index=st;
+                    // st.pop();
+                    st++;
                 }
                 swap(nums[i],nums[index]);
                 sort(nums.begin()+i+1,nums.end());
