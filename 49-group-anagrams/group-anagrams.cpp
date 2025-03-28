21dd4e213a554a648e0c271309b948a0327e37c5
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2025-03-28 23:47:07 +0530

    Time: 115 ms (5.01%) | Memory: 58.3 MB (5.45%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        unordered_map<string,vector<string>> dp;
+        for(int i=0;i<strs.size();i++){
+            unordered_map<char,int> m;
+            for(auto c:strs[i]){
+                m[c]++;
+            }
+            string s="";
+            for(int i=0;i<26;i++){
+                s+=m[i+'a'];
+            }
+            dp[s].push_back(strs[i]);
+        }
+
+        for(auto p:dp){
+            ans.push_back(p.second);
+        }
+
+        return ans;
+    }
+};
