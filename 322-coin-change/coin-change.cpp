a2ddbcd2aad9fab550bb7c26d6104c38a7e727e9
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2025-04-03 00:57:47 +0530

    Time: 55 ms (19.73%) | Memory: 48.8 MB (39.60%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -22,11 +22,40 @@ public:
     }
     int coinChange(vector<int>& coins, int amount) {
         vector<vector<int>> dp(coins.size(),vector<int> (amount+1,-1));
-        int val=getans(coins,coins.size()-1,amount,dp);
+        // int val=getans(coins,coins.size()-1,amount,dp);
+        // if(val>10000){
+        //     return -1;
+        // }
+
+        // return val;
+
+        for(int amt=0;amt<=amount;amt++){
+            if(amt%coins[0]==0){
+                dp[0][amt]=amt/coins[0];
+            }
+            else{
+                dp[0][amt]=1e9;
+            }
+        }
+
+        for(int i=1;i<coins.size();i++){
+            for(int amt=0;amt<=amount;amt++){
+                int w1=INT_MAX;
+                if(coins[i]<=amt){
+                    w1=1+dp[i][amt-coins[i]];
+                }
+                int w2=dp[i-1][amt];
+
+                dp[i][amt]=min(w1,w2);
+            }
+        }
+
+        int val=dp[coins.size()-1][amount];
         if(val>10000){
             return -1;
         }
 
         return val;
+
     }
 };
