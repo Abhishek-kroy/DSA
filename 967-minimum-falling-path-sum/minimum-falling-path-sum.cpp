28f178482e9d7a0c7222b61fdd583b56e9cd5ec8
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2025-04-02 20:12:58 +0530

    Time: 3 ms (33.50%) | Memory: 15 MB (39.72%) - LeetSync

---
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        vector<int> dp=matrix.back();
+
+        for(int level=matrix.size()-2;level>=0;level--){
+            vector<int> temp(matrix[0].size());
+            for(int j=0;j<matrix[0].size();j++){
+                temp[j]=matrix[level][j]+min({dp[j],j+1<matrix.size() ? dp[j+1]:INT_MAX,j>0 ? dp[j-1]: INT_MAX});
+            }
+            dp=temp;
+        }
+
+        int ans=INT_MAX;
+
+        for(auto v:dp){
+            ans=min(ans,v);
+        }
+
+        return ans;
+    }
+};
