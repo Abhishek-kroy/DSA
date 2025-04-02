87d3059fe51add24d196770cc42b48d7a9e74049
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2025-04-03 00:47:13 +0530

    Time: 48 ms (28.78%) | Memory: 49 MB (33.58%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    map<pair<int,int>,int> dp;
-    int getans(vector<int> &coins,int i,int amount){
+    // map<pair<int,int>,int> dp;
+    int getans(vector<int> &coins,int i,int amount,vector<vector<int>> &dp){
         if(i==0){
             if(amount%coins[i]==0)  return amount/coins[i];
             return 1e9;
         }
 
-        if(dp.find({i,amount})!=dp.end()){
-            return dp[{i,amount}];
+        if(dp[i][amount]!=-1){
+            return dp[i][amount];
         }
 
         int w1=INT_MAX;
         if(coins[i]<=amount){
-            w1=1+getans(coins,i,amount-coins[i]);
+            w1=1+getans(coins,i,amount-coins[i],dp);
         }
 
-        int w2=getans(coins,i-1,amount);
+        int w2=getans(coins,i-1,amount,dp);
 
-        return dp[{i,amount}]=min(w1,w2);
+        return dp[i][amount]=min(w1,w2);
     }
     int coinChange(vector<int>& coins, int amount) {
-        int val=getans(coins,coins.size()-1,amount);
+        vector<vector<int>> dp(coins.size(),vector<int> (amount+1,-1));
+        int val=getans(coins,coins.size()-1,amount,dp);
         if(val>10000){
             return -1;
         }
