0b04c53b090efd338601c0170682b185cb0d1bb1
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2025-04-02 20:02:53 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (46.35%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,23 +1,39 @@
 class Solution {
-        map<pair<int,int>,int> dp;
-    int getans(vector<vector<int>>& triangle,int level,int i){
-        if(level>triangle.size()){
-            return 0;
-        }
+    //     map<pair<int,int>,int> dp;
+    // int getans(vector<vector<int>>& triangle,int level,int i){
+    //     if(level>triangle.size()){
+    //         return 0;
+    //     }
 
-        if(dp.find({level,i})!=dp.end()){
-            return dp[{level,i}];
-        }
+    //     if(dp.find({level,i})!=dp.end()){
+    //         return dp[{level,i}];
+    //     }
 
-        // take i
-        int w1=triangle[level-1][i]+getans(triangle,level+1,i);
-        // take i+1
-        int w2=triangle[level-1][i+1]+getans(triangle,level+1,i+1);
+    //     // take i
+    //     int w1=triangle[level-1][i]+getans(triangle,level+1,i);
+    //     // take i+1
+    //     int w2=triangle[level-1][i+1]+getans(triangle,level+1,i+1);
 
-        return dp[{level,i}]=min(w1,w2);
-    }
+    //     return dp[{level,i}]=min(w1,w2);
+    // }
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        return triangle[0][0]+getans(triangle,2,0);
+        vector<vector<int>> dp(triangle.size(),vector<int> (triangle.back().size(),-1));
+        for(int j=0;j<triangle.back().size();j++){
+            dp[triangle.size()-1][j]=triangle[triangle.size()-1][j];
+        }
+
+        for(int level=triangle.size()-2;level>=0;level--){
+            for(int j=0;j<=level;j++){
+                dp[level][j]=triangle[level][j]+min(dp[level+1][j],dp[level+1][j+1]);
+            }
+        }
+        // 2
+        // 3 4
+        // 6 5 7
+        // 4 1 8 3
+
+        return dp[0][0];
+        // return triangle[0][0]+getans(triangle,2,0);
     }
 };
