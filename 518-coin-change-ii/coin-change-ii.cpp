426d42beaa986c653fee2b9ddcdd1e85315bd81d
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2025-04-03 10:07:48 +0530

    Time: 31 ms (28.59%) | Memory: 46.1 MB (13.92%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -24,8 +24,29 @@ public:
         return dp[i][amount]=w1+w2;
     }
     int change(int amount, vector<int>& coins) {
-        vector<vector<int>> dp(coins.size(),vector<int> (amount+1,-1));
-        return getans(amount,coins,coins.size()-1,dp);
+        vector<vector<long>> dp(coins.size(),vector<long> (amount+1,0));
+        // return getans(amount,coins,coins.size()-1,dp);
+        for(int amt=0;amt<=amount;amt++){
+            if(amt%coins[0]==0){
+                dp[0][amt]=1;
+            }
+            else{
+                dp[0][amt]=0;
+            }
+        }
+
+
+        for(int i=1;i<coins.size();i++){
+            for(int amt=0;amt<=amount;amt++){
+                int w1=0;
+                if(coins[i]<=amt)
+                w1=dp[i][amt-coins[i]];
+                int w2=dp[i-1][amt];
+                dp[i][amt]=1LL*w1+1LL*w2;
+            }
+        }
+
+        return dp[coins.size()-1][amount];
 
     }
 };
