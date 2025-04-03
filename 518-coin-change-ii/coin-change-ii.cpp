70988ad8b9aa7ea1fc76b88a02985ced9d0a176b
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2025-04-03 10:02:08 +0530

    Time: 15 ms (75.48%) | Memory: 28.3 MB (62.87%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int getans(int amount,vector<int>& coins,int i,vector<vector<int>> &dp){
-        if(amount<0){
-            return 0;
-        }
+        // if(amount<0){
+        //     return 0;
+        // }
 
         if(i==0){
             if(amount%coins[0]==0){
@@ -17,12 +17,15 @@ public:
             return dp[i][amount];
         }
 
-        int w1=getans(amount-coins[i],coins,i,dp);
+        int w1=0;
+        if(coins[i]<=amount)
+        w1=getans(amount-coins[i],coins,i,dp);
         int w2=getans(amount,coins,i-1,dp);
         return dp[i][amount]=w1+w2;
     }
     int change(int amount, vector<int>& coins) {
         vector<vector<int>> dp(coins.size(),vector<int> (amount+1,-1));
         return getans(amount,coins,coins.size()-1,dp);
+
     }
 };
