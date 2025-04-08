606131d6e9a6c9c3a0e9de48e0a04a4bb3ed04c9
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2025-04-08 12:59:33 +0530

    Time: 3 ms (31.91%) | Memory: 10.3 MB (16.15%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -13,7 +13,20 @@ public:
     }
     int subsetXORSum(vector<int>& nums) {
         int sum=0;
-        getans(nums,0,0,sum);
+        // getans(nums,0,0,sum);
+        // return sum;
+        vector<int> v;
+        int n=nums.size();
+        for(int i=n-1;i>=0;i--){
+            int k=v.size();
+            for(int j=0;j<k;j++){
+                v.push_back(nums[i]^v[j]);   
+                sum+=nums[i]^v[j];
+            }
+            sum+=nums[i];
+            v.push_back(nums[i]);
+        }
+
         return sum;
     }
 };
