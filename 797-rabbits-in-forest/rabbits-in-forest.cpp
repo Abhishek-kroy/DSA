8c0528ce99bccec4506b4a42f7c6ba9a94247df0
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2025-04-20 10:05:33 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (91.60%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.cpp
+++ 797-rabbits-in-forest/rabbits-in-forest.cpp
@@ -1,21 +1,33 @@
 class Solution {
 public:
     int numRabbits(vector<int>& answers) {
-        int n=answers.size();
+        // int n=answers.size();
+        // int ans=0;
+        // for(int i=0;i<answers.size();i++){
+        //     if(answers[i]==-1)  continue;
+
+        //     int oth=answers[i]+1;
+        //     int v=answers[i];
+        //     ans+=oth;
+
+        //     for(int j=i;j<n && oth;j++){
+        //         if(v==answers[j]){
+        //             answers[j]=-1;
+        //             oth--;
+        //         }
+        //     }
+        // }
+
+
+        unordered_map<int,int> dp;
+        for(auto v:answers){
+            dp[v+1]++;
+        }
+
         int ans=0;
-        for(int i=0;i<answers.size();i++){
-            if(answers[i]==-1)  continue;
 
-            int oth=answers[i]+1;
-            int v=answers[i];
-            ans+=oth;
-
-            for(int j=i;j<n && oth;j++){
-                if(v==answers[j]){
-                    answers[j]=-1;
-                    oth--;
-                }
-            }
+        for(auto [n,f]:dp){
+            ans+=(((f+n-1)/n)*n);
         }
 
         return ans;
