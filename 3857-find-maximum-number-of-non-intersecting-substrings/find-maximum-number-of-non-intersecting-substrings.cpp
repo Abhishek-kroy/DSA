dcae0cad9bd647ebdbb6f60e07a591c7195b5f0a
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2025-05-28 23:29:57 +0530

    Time: 146 ms (62.76%) | Memory: 56.2 MB (62.20%) - LeetSync

--- 3857-find-maximum-number-of-non-intersecting-substrings/find-maximum-number-of-non-intersecting-substrings.cpp
+++ 3857-find-maximum-number-of-non-intersecting-substrings/find-maximum-number-of-non-intersecting-substrings.cpp
@@ -1,20 +1,30 @@
+#include <algorithm> // for lower_bound
+
 class Solution {
 public:
     int maxSubstrings(string word) {
-        unordered_map<char,int> m;
-        int ans=0;
-        for(int i=0;i<word.size();i++){
-            if(m.find(word[i])==m.end()){
-                m[word[i]]=i;
-            }
-            else{
-                int j=m[word[i]];
-                if(i-j>=3){
-                    ans++;
-                    m.clear();
-                }
-            }
+        int n = word.size();
+        vector<int> dp(n + 1, 0); // dp[i] = max substrings starting from index i
+        vector<vector<int>> adj(26);
+        
+        for (int i = 0; i < n; i++) {
+            adj[word[i] - 'a'].push_back(i);
         }
-        return ans;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int ch = word[i] - 'a';
+            int target = i + 3;
+            auto it = lower_bound(adj[ch].begin(), adj[ch].end(), target);
+            int cnt = 0;
+
+            if (it != adj[ch].end()) {
+                int j = *it;
+                cnt = 1 + dp[j + 1];
+            }
+
+            dp[i] = max(cnt, dp[i + 1]);
+        }
+
+        return dp[0];
     }
 };
