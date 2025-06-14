3fc282318cb0a729b3a00d1a7d53400dbd1b24f3
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2025-06-14 23:32:12 +0530

    Time: 53 ms (97.69%) | Memory: 19.3 MB (83.68%) - LeetSync

--- 464-can-i-win/can-i-win.cpp
+++ 464-can-i-win/can-i-win.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    unordered_map<int, bool> dp;
+    vector<char> dp;
 
     bool getans(int maxChoosableInteger, int desiredTotal, int used) {
-        if (dp.count(used)) return dp[used];
+        if (dp[used] != -1) return dp[used];
 
         for (int i = 0; i < maxChoosableInteger; i++) {
-            if (!(used & (1 << i))) { // number (i+1) not used
+            if (!(used & (1 << i))) {
                 if (desiredTotal <= i + 1 || 
                     !getans(maxChoosableInteger, desiredTotal - (i + 1), used | (1 << i))) {
-                    return dp[used] = true; // current player wins
+                    return dp[used] = 1;
                 }
             }
         }
 
-        return dp[used] = false; // no winning move
+        return dp[used] = 0;
     }
 
     bool canIWin(int maxChoosableInteger, int desiredTotal) {
@@ -23,6 +23,7 @@ public:
         if (sum < desiredTotal) return false;
         if (desiredTotal <= 0) return true;
 
-        return getans(maxChoosableInteger, desiredTotal, 0); // 0 → no numbers used yet
+        dp.assign(1 << maxChoosableInteger, -1);
+        return getans(maxChoosableInteger, desiredTotal, 0);
     }
 };
