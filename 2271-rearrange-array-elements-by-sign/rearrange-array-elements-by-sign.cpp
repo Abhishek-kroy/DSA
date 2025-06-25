8a2fbe620abc73848685816d9e844ea255e09ec3
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2025-06-25 23:38:34 +0530

    Time: 11 ms (33.02%) | Memory: 128.2 MB (79.15%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        queue<int> p;
-        queue<int> n;
-        for(auto v:nums){
-            if(v>0){
-                p.push(v);
+        int p=0;
+        int n=1;
+        vector<int> ans(nums.size());
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>0){
+                ans[p]=nums[i];
+                p+=2;
             }
             else{
-                n.push(v);
+                ans[n]=nums[i];
+                n+=2;
             }
-        }
-        int nu=nums.size()/2;
-        vector<int> ans(2*nu);
-        for(int i=0;i<2*nu;i+=2){
-            ans[i]=p.front();
-            p.pop();
-            ans[i+1]=n.front();
-            n.pop();
         }
         return ans;
     }
