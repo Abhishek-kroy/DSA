853f055fa98caceed3cb04e511d9e3f0c1835953
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2025-07-08 09:32:43 +0530

    Time: 195 ms (21.16%) | Memory: 123.6 MB (51.85%) - LeetSync

--- 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
-    
-    unordered_map<int,int> bsdp;
-
     int bs(vector<vector<int>>& events, int val) {
-        if(bsdp.find(val)!=bsdp.end()) return bsdp[val];
         int l = 0, r = events.size() - 1;
         while (l <= r) {
             int mid = l + (r - l) / 2;
@@ -14,26 +10,24 @@ public:
                 r = mid - 1;
             }
         }
-        return bsdp[val]=l;
+        return l;
     }
 
-    unordered_map<int, unordered_map<int, int>> dp;
-
-    int getans(vector<vector<int>>& events, int k, int i) {
+    int getans(vector<vector<int>>& events, vector<vector<int>>& dp, int k, int i) {
         if (k == 0 || i >= events.size()) return 0;
-
-        if (dp[i].count(k)) return dp[i][k];
+        if (dp[i][k] != -1) return dp[i][k];
 
         int next = bs(events, events[i][1] + 1);
-        int pick = events[i][2] + getans(events, k - 1, next);
-
-        int skip = getans(events, k, i + 1);
+        int pick = events[i][2] + getans(events, dp, k - 1, next);
+        int skip = getans(events, dp, k, i + 1);
 
         return dp[i][k] = max(pick, skip);
     }
 
     int maxValue(vector<vector<int>>& events, int k) {
         sort(events.begin(), events.end());
-        return getans(events, k, 0);
+        int n = events.size();
+        vector<vector<int>> dp(n, vector<int>(k + 1, -1));
+        return getans(events, dp, k, 0);
     }
 };
