8c01184fde286d07bba2006f205347dba9cc9e15
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2025-07-10 10:31:23 +0530

    Time: 4 ms (10.23%) | Memory: 11.7 MB (5.62%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,37 +1,30 @@
 class Solution {
 public:
     unordered_map<int,int> dp;
-    int getans(string s,int i=0){
-        if(s[i]=='0') return 0;
-        if(i>=s.size()){
+    int getans(string s,int i){
+        int n=s.size();
+        if(i>=n){
             return 1;
         }
 
-        if(dp.find(i)!=dp.end()){
-            return dp[i];
+        if(dp.find(i)!=dp.end())    return dp[i];
+        
+        if(s[i]=='0'){
+            return 0;
         }
 
-        int w1=0;
-
-        if(i==s.size()-1){
-            w1+=getans(s,i+1);
+        int w1=getans(s,i+1);
+        int w2=0;
+        if(s[i]=='1' && (i+1)<n){
+            w2=getans(s,i+2);
         }
-        else if(s[i]=='1'){
-            w1+=getans(s,i+1);
-            w1+=getans(s,i+2);
-        }
-        else if(s[i]=='2'){
-            w1+=getans(s,i+1);
-            if(s[i+1]>='0' and s[i+1]<='6')
-                w1+=getans(s,i+2);
-        }
-        else{
-            w1+=getans(s,i+1);
+        if(s[i]=='2' && (i+1)<n && s[i+1]<='6'){
+            w2=getans(s,i+2);
         }
 
-        return dp[i]=w1;
+        return dp[i]=w1+w2;
     }
     int numDecodings(string s) {
-        return getans(s,0)   ;
+        return getans(s,0);
     }
 };
