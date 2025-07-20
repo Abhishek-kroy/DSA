abc5b8da7cf473c3712bc752678e30a751fcd481
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2025-07-20 23:43:17 +0530

    Time: 4 ms (69.58%) | Memory: 23.9 MB (57.40%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -2,27 +2,19 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(),intervals.end());
-        int st=intervals[0][0];
-        int end=intervals[0][1];
         int n=intervals.size();
         vector<vector<int>> ans;
-        for(int i=0;i<n;i++){
-            if(st==-1){
-                st=intervals[i][0];
-                end=intervals[i][1];
-                continue;
+        int i=0;
+        while(i<n){
+            int st=intervals[i][0];
+            int end=intervals[i][1];
+            while(i<n && intervals[i][0]<=end){
+                end=max(end,intervals[i][1]);
+                i++;
             }
-            if(intervals[i][0]<=end){
-                end=max(intervals[i][1],end);
-            }
-            else{
-                ans.push_back({st,end});
-                st=intervals[i][0];
-                end=intervals[i][1];
-            }
+
+            ans.push_back({st,end});
         }
-        
-        ans.push_back({st,end});
 
         return ans;
 
