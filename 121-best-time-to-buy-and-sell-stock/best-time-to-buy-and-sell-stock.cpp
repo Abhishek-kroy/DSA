7dd13bc7ed223ba10fb23e4e8f00ad86e95ff8d1
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2025-07-20 18:04:09 +0530

    Time: 0 ms (100.00%) | Memory: 97.3 MB (60.29%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        int v=prices[n-1];
         int ans=0;
-        for(int i=n-2;i>=0;i--){
-            ans=max(ans,v-prices[i]);
-            v=max(v,prices[i]);
+        int n=prices.size();
+        int prev=prices[0];
+        for(int i=1;i<n;i++){
+            ans=max(ans,prices[i]-prev);
+            prev=min(prev,prices[i]);
         }
 
         return ans;
