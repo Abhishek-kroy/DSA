0a5e8966e7ce4190286ab39cbf3e878cc4b0cc9b
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2025-07-20 19:13:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (18.29%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,41 +1,27 @@
 class Solution {
 public:
-    // unordered_map<int,int> dp;
-    // int getans(int i,int n){
-    //     if(i==n){
-    //         return 1;
-    //     }
-
-    //     // if(dp.find(i)!=dp.end()){
-    //         // return dp[i];
-    //     // }
-
-    //     int w1=0,w2=0;
-        
-    //     if(i+1<=n)
-    //     w1=getans(i+1,n);
-    //     if(i+2<=n)
-    //     w2=getans(i+2,n);
-
-    //     // return dp[i]=w1+w2;
-    // }
-
-    int climbStairs(int n) {
-        // 0 1 2
-        // 0 1 0
-        vector<int> dp(n + 1, 0);
-        dp[n]=1;
-
-    for (int i = n - 1; i >= 0; i--) {
-        if ((i + 1) <= n) {
-            dp[i] = dp[i + 1];
+    int getans(int n,vector<int>& dp){
+        if(n==0){
+            return 1;
         }
-        if ((i + 2) <= n) {
-            dp[i] += (dp[i + 2]);
+
+        if(dp[n]!=-1){
+            return dp[n];
         }
+
+        long long w1=0,w2=0;
+
+        w1=getans(n-1,dp);
+
+        if(n>1){
+            w2=getans(n-2,dp);
+        }
+
+        return dp[n]=w1+w2;
     }
+    int climbStairs(int n) {
 
-    return dp[0];
-        // return getans(0,n);
+        vector<int> dp(n+1,-1);
+        return getans(n,dp);
     }
 };
