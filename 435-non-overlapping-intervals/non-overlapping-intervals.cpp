eee348ccac208bf29da6fd15f439f6c476fcb0df
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2025-07-21 00:14:41 +0530

    Time: 141 ms (17.00%) | Memory: 95.9 MB (25.72%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -1,27 +1,44 @@
 class Solution {
 public:
-    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end(),[](vector<int>& a,vector<int>& b){
-            return a[1]<b[1];
-        });
-
-        for(auto i:intervals){
-            cout<<i[0]<<" "<<i[1]<<endl;
+    static bool cmp(vector<int>& a,vector<int>& b){
+        return a[1]<b[1];
+    }
+    int getans(vector<vector<int>>& intervals,int i){
+        int l=0;
+        int r=i-1;
+        int ans=-1;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(intervals[mid][1]<=intervals[i][0]){
+                ans=mid;
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
         }
-
-        int cnt=0;
-        for(int i=0;i<intervals.size();i++){
-            int st=intervals[i][0];
-            int end=intervals[i][1];
-            int j=i+1;
-            while(j<intervals.size() and intervals[j][0]<end){
-                cnt++;
-                j++;
+        return ans;
+    }
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end(),cmp);
+        int n=intervals.size();
+        vector<int> dp(n,0);
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int j=getans(intervals,i);
+            int w1=1;
+            if(j!=-1){
+                w1+=dp[j];
+            }
+            if(i>0)
+                dp[i]=max(dp[i-1],w1);
+            else{
+                dp[i]=w1;     
             }
 
-            i=j-1;
+            ans=max(ans,dp[i]);
         }
 
-        return cnt;
+        return n-dp[n-1];
     }
 };
