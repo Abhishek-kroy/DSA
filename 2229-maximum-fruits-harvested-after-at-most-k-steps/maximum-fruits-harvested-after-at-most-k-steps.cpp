07f864587d991bf55b093fa9fc1908cd19490e23
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2025-08-04 00:57:14 +0530

    Time: 0 ms (100.00%) | Memory: 124.5 MB (84.68%) - LeetSync

---
+++ 2229-maximum-fruits-harvested-after-at-most-k-steps/maximum-fruits-harvested-after-at-most-k-steps.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int steps(int l,int r,int st,vector<vector<int>>& fruits){
+        if(fruits[r][0]<st){
+            return st-fruits[l][0];
+        }
+        else if(st<fruits[l][0]){
+            return fruits[r][0]-st;
+        }
+
+        return min(st-fruits[l][0]+(fruits[r][0]-fruits[l][0]),fruits[r][0]-st+(fruits[r][0]-fruits[l][0]));
+    }
+    int maxTotalFruits(vector<vector<int>>& fruits, int startPos, int k) {
+        int l=0;
+        int r=0;
+        int n=fruits.size();
+
+        int ans=0;
+
+        int sum =0 ;
+
+        while(r<n){
+
+            sum+=fruits[r][1];
+
+            while(l<=r && steps(l,r,startPos,fruits)>k){
+                sum-=fruits[l][1];
+                l++;
+            }
+            
+
+            ans=max(ans,sum);
+            r++;
+
+        }
+
+        return ans;
+
+
+
+    }
+};
