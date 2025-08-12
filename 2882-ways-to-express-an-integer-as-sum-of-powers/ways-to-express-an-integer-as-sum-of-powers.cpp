f2424c04de9b616e9aec42ff5665980aabd5d74a
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2025-08-12 10:52:51 +0530

    Time: 414 ms (20.48%) | Memory: 14.4 MB (64.05%) - LeetSync

--- 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -1,23 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-    map<pair<int,int>,int> dp;
-    const int MOD = 1e9 + 7 ;
-    int getans(int n,int x,int num){
-        if(n==0){
-            return 1;
+    static map<pair<int,pair<int,int>>, int> dp;
+    const int MOD = 1e9 + 7;
+
+    long long ipow(int base, int exp) {
+        long long result = 1;
+        while (exp--) {
+            result *= base;
         }
-        if(n<0 || pow(num,x)>n){
-            return 0;
-        }
-
-        if(dp.find({n,num})!=dp.end())  return dp[{n,num}];
-
-        int w1=getans(n-pow(num,x),x,num+1);
-        int w2=getans(n,x,num+1);
-
-        return dp[{n,num}]=(w1+w2)%MOD;
+        return result;
     }
+
+    int getans(int n, int x, int num) {
+        if (n == 0) return 1;
+        long long p = ipow(num, x);
+        if (n < 0 || p > n) return 0;
+
+        if (dp.find({n,{num,x}}) != dp.end())
+            return dp[{n,{num,x}}];
+
+        int w1 = getans(n - p, x, num + 1);
+        int w2 = getans(n, x, num + 1);
+
+        return dp[{n,{num,x}}] = (w1 + w2) % MOD;
+    }
+
     int numberOfWays(int n, int x) {
-        return getans(n,x,1);
+        for (int i = 1; i <= 300; i++) {
+            for (int y = 1; y <= 5; y++) {
+                getans(i, y, 1);
+            }
+        }
+        return dp[{n,{1,x}}];
     }
 };
+
+map<pair<int,pair<int,int>>, int> Solution::dp;
