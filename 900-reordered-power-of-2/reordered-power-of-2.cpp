a83ec463ddd491858fd2f44beae80a508bf3de8f
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2025-08-15 11:44:15 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (91.08%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.cpp
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -1,13 +1,35 @@
 class Solution {
+    static unordered_set<string> s;
 public:
-    bool reorderedPowerOf2(int n) {
-        string s=to_string(n);
-        sort(s.begin(),s.end());
-        do{
-            int num=stoi(s);
-            if(s[0]!='0' && (num&(num-1))==0)  return true;
-        }   while(next_permutation(s.begin(),s.end()));
-        return false;
+    string generate(int num){
+        vector<int> f(10);
+        while(num){
+            int d=num%10;
+            f[d]++;
+            num/=10;
+        }
+
+        string str="";
+
+        for(int i=0;i<10;i++){
+            str+=f[i];
+        }
+
+        return str;
+
     }
 
+    void process(){
+        if(s.size()!=0) return ;
+        for(int i=1;i<=1e9;i<<=1){
+            string str=generate(i);
+            s.insert(str);
+        }
+    }
+    bool reorderedPowerOf2(int n) {
+        process();
+        return s.find(generate(n))!=s.end();
+    }
 };
+
+unordered_set<string> Solution::s;
