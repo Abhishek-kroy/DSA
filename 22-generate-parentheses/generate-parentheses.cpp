8f03aa9adf92a1d26db45631a22157c6e50d59ab
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2025-08-29 11:26:39 +0530

    Time: 3 ms (71.64%) | Memory: 15.6 MB (64.49%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-    unordered_set<string> st;
-    void getans(int n,string s,int o,int c){
-        if(n==0){
-            if(o==c){
-                st.insert(s);
-            }
+    void getans(int len,int o,string s,vector<string>& ans){
+        if(len==0){
+            ans.push_back(s);
             return ;
         }
 
-        if(c>o) return ;
+        // can we add (
+        if((len-o)>0){
+            getans(len-1,o+1,s+"(",ans);
+        }
+        if(o){
+            getans(len-1,o-1,s+")",ans);
+        }
 
-        getans(n-1,s+"(",o+1,c);
-        getans(n-1,s+")",o,c+1);
+
+
     }
     vector<string> generateParenthesis(int n) {
-        getans(2*n,"",0,0);
-        
-        return vector<string> (st.begin(),st.end());
+        vector<string> ans;
+        getans(2*n,0,"",ans);
+        return ans;
     }
 };
