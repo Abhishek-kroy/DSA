823702138a2824a33c9753765e1735183215346f
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2025-12-27 19:38:49 +0530

    Time: 304 ms (5.49%) | Memory: 91.8 MB (78.02%) - LeetSync

--- 2002-stone-game-viii/stone-game-viii.cpp
+++ 2002-stone-game-viii/stone-game-viii.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
     int stoneGameVIII(vector<int>& stones) {
         int n=stones.size();
+        cout<<stones[0]<<" ";
         for(int i=1;i<n;i++){
             stones[i]+=stones[i-1];
+            cout<<stones[i]<<" ";   
         }
 
         // so need a-b value to be optimal 
@@ -11,16 +13,17 @@ public:
         // so ans tell from i to n-1 what is optimal value to take  
         // if a split i then b would optimally play so ans   
 
-        int ans=stones[n-1];  
+        // int ans=stones[n-1];  
+
+        vector<int> dp(n,0); 
+        dp[n-1]=stones[n-1];
 
         for(int i=n-2;i>0;i--){
             // takes i+1 stones 
-            int alice=stones[i]; 
-            int b=ans;     
-
-            ans=max(ans,alice-b);     
+            int alice=stones[i];     
+            dp[i]=max(dp[i+1],alice-dp[i+1]);        
         }
 
-        return ans; 
+        return dp[1];   
     }
 };
