f9578ee62b4c1b31ebfecf19a067823ae3b0ab98
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2026-01-09 11:32:17 +0530

    Time: 420 ms (7.99%) | Memory: 63.6 MB (27.84%) - LeetSync

---
+++ 4021-distinct-points-reachable-after-substring-removal/distinct-points-reachable-after-substring-removal.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int distinctPoints(string s, int k) {
+        int n = s.size();
+        vector<int> netR(n, 0), netU(n, 0);
+
+        netR[0] = (s[0] == 'R' ? 1 : (s[0] == 'L' ? -1 : 0));
+        netU[0] = (s[0] == 'U' ? 1 : (s[0] == 'D' ? -1 : 0));
+
+        for (int i = 1; i < n; i++) {
+            netR[i] = netR[i - 1] + (s[i] == 'R' ? 1 : (s[i] == 'L' ? -1 : 0));
+            netU[i] = netU[i - 1] + (s[i] == 'U' ? 1 : (s[i] == 'D' ? -1 : 0));
+        }
+
+        set<string> st;
+
+        for (int i = 0; i <= n - k; i++) {
+            int r = (i > 0 ? netR[i - 1] : 0) + (netR[n - 1] - netR[i + k - 1]);
+            int u = (i > 0 ? netU[i - 1] : 0) + (netU[n - 1] - netU[i + k - 1]);
+
+            st.insert(to_string(r) + "," + to_string(u));
+        }
+
+        return st.size();
+    }
+}; 
