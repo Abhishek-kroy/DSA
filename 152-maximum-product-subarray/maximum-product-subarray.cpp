fe3c4a9043677753826189b57121c5f801ba3578
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2026-02-02 09:36:59 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (85.06%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,23 +1,55 @@
 class Solution {
 public:
-    int maxProduct(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 1) return nums[0];
-
-        int maxProd = INT_MIN;
-        int prefix = 1, suffix = 1;
-
-        for (int i = 0; i < n; i++) {
-            // If prefix becomes 0, reset it to 1
-            prefix = (prefix == 0) ? 1 : prefix;
-            suffix = (suffix == 0) ? 1 : suffix;
-
-            prefix *= nums[i];         // Forward traversal
-            suffix *= nums[n - 1 - i]; // Backward traversal
-
-            maxProd = max(maxProd, max(prefix, suffix));
+    int getans(vector<int>& nums,int i,int j,int negcount,int left,int right){
+        if(j<i){
+            return INT_MIN;   
         }
 
-        return maxProd;
+        if(negcount%2==0){
+            int cnt=1;
+            for(int k=i;k<=j;k++){
+                cnt*=nums[k];
+            }
+
+            return cnt;  
+        }
+        return max(getans(nums,i,right-1,negcount-1,left,right),getans(nums,left+1,j,negcount-1,left,right))   ; 
+    }
+
+    int maxProduct(vector<int>& nums) {
+        nums.push_back(0);
+        int l=0;
+        int r=0;
+        int n=nums.size();
+
+        int ans=nums[0]; 
+        int neg=0;
+        int left=-1;
+        int right=-1;
+
+        while(r<n){
+            if(nums[r]==0){
+                ans=max(ans,getans(nums,l,r-1,neg,left,right));
+                if(r!=n-1)
+                    ans=max(ans,0);   
+                l=r+1;
+                neg=0;
+                left=-1;
+                right=-1;
+            }
+            if(nums[r]<0){  
+                neg++;
+                if(left==-1){
+                    left=r;
+                    right=r;
+                }
+                else{
+                    right=r;
+                }
+            }
+            r++; 
+        }
+
+        return ans;
     }
 };
