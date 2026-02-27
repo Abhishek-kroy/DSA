0d8ce0209b7f821bc3a346d97ebf250e9b8cb790
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2026-02-28 01:38:24 +0530

    Time: 8 ms (36.65%) | Memory: 11.1 MB (7.09%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -3,19 +3,19 @@ public:
     int characterReplacement(string s, int k) {
         int n=s.size();
         int i=0;
-        int f=0;
+        int j=0;
         int ans=0;
-        vector<int> freq(26,0);
-        for(int j=0;j<n;j++){
-            freq[s[j]-'A']++;
-            f=max(f,freq[s[j]-'A']);
-            int ch=(j-i+1)-f;
-            while((j-i+1)-f>k){
-                freq[s[i]-'A']--;
+        unordered_map<char,int> fq;
+        int f=0;
+        while(j<n){
+            fq[s[j]]++;
+            f=max(f,fq[s[j]]); 
+            while(((j-i+1)-f)>k){  
+                fq[s[i]]--;  
                 i++;
             }
-
-            ans=max(ans,j-i+1);
+            ans=max(ans,(j-i+1));
+            j++;        
         }
 
         return ans; 
