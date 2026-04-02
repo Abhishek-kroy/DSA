e20f4c962ce66e7cf95b04581b76b1a754059433
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2026-04-02 22:18:42 +0530

    Time: 428 ms (66.81%) | Memory: 108.3 MB (83.83%) - LeetSync

--- 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
+++ 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    vector<vector<vector<int>>> dp;
+    int dp[500][500][3];
+    int INF = -1e9;
 
     int getans(vector<vector<int>>& coins,int i,int j,int k){
         int n=coins.size();                                        
@@ -10,10 +11,10 @@ public:
             return -1e8;
         }
 
-        if(dp[i][j][k]!=-1e8) return dp[i][j][k];
+        if(dp[i][j][k]!=INF) return dp[i][j][k];
 
         if(i==n-1 && j==m-1){
-            if(coins[i][j]>=0){   
+            if(coins[i][j]>=0){
                 return dp[i][j][k]=coins[i][j];
             }
 
@@ -26,9 +27,13 @@ public:
 
         int w1=-1e8;
 
-        w1=max({w1,coins[i][j]+getans(coins,i+1,j,k),coins[i][j]+getans(coins,i,j+1,k)});
+        w1=max({w1,coins[i][j]+getans(coins,i+1,j,k),
+                     coins[i][j]+getans(coins,i,j+1,k)});
+
         if(k>0 && coins[i][j]<0){
-            w1=max({w1,0+getans(coins,i+1,j,k-1),0+getans(coins,i,j+1,k-1)});   
+            w1=max({w1,
+                    getans(coins,i+1,j,k-1),
+                    getans(coins,i,j+1,k-1)});
         }
 
         return dp[i][j][k]=w1;   
@@ -37,7 +42,12 @@ public:
     int maximumAmount(vector<vector<int>>& coins) {
         int n=coins.size();
         int m=coins[0].size();
-        dp.assign(n,vector<vector<int>>(m,vector<int>(3,-1e8)));
+
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+                for(int k=0;k<3;k++)
+                    dp[i][j][k]=INF;
+
         return getans(coins,0,0,2);
     }
 };   
