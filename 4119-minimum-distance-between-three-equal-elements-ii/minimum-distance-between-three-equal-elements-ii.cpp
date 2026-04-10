72fca09ee84c04aa0c257412f1de2aee93b4860d
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2026-04-10 09:37:44 +0530

    Time: 8 ms (98.76%) | Memory: 224.8 MB (98.02%) - LeetSync

---
+++ 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minimumDistance(vector<int>& nums) {
+
+        int n=nums.size();
+
+        vector<pair<int,int>> f(n+1,{-1,-1});   
+
+        int ans=INT_MAX;
+
+        for(int i=0;i<n;i++){
+            int val=nums[i];
+
+            if(f[val].first==-1){
+                f[val].first=i;
+            }
+            else if(f[val].second==-1){
+                f[val].second=i;
+            }
+            else{
+                int i1=f[val].first;
+                int i2=f[val].second;
+                int i3=i;   
+                ans=min(ans,i2-i1+i3-i2+i3-i1);
+                f[val].first=i2;
+                f[val].second=i3;
+            }
+        }
+
+
+        if(ans==INT_MAX)return -1;
+        return ans;
+    }
+};   
