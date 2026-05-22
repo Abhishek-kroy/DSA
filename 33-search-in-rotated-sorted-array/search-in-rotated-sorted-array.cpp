cc4be4047249233d4732900779ab0085cd05aaae
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2026-05-22 06:41:22 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (33.81%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -2,36 +2,40 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int n=nums.size();
+
         int l=0;
+
         int r=n-1;
 
         int ans=-1;
 
         while(l<=r){
-            int mid=(l+r)/2;
+            int mid=l+(r-l)/2;
+
             if(nums[mid]==target){
                 ans=mid;
-                break; 
+                break;    
             }
 
-            if(nums[l]<=nums[mid]){
-                if(target>=nums[l] && target<nums[mid]){
-                    r=mid-1;  
+            else if(nums[l]<=nums[mid]){
+                if(target>=nums[l] && target<=nums[mid]){
+                    r=mid-1;
                 }
                 else{
                     l=mid+1;
                 }
             }
-            else{
-                if(target>=nums[mid] && target<=nums[r]){        
-                    l=mid+1;                    
+
+            else {
+                if(target>=nums[mid] && target<=nums[r]){
+                    l=mid+1;
                 }
                 else{
                     r=mid-1;
-                }  
+                }
             }
         }
 
-        return ans;  
+        return ans; 
     }
 };
