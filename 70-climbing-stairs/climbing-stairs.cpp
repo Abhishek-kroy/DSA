4c9f71266520a8114c0d06ed4728696858120280
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2026-05-25 20:08:09 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (69.57%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-    int getans(int n,vector<int>& dp){
-        if(n==0){
+    int dp[46];    
+    int getans(int i,int n){
+        if(i==n){
             return 1;
         }
 
-        if(dp[n]!=-1){
-            return dp[n];
+        if(dp[i]!=-1){
+            return dp[i];
         }
 
-        long long w1=0,w2=0;
+        int w1=getans(i+1,n); 
+        int w2=0;
 
-        w1=getans(n-1,dp);
-
-        if(n>1){
-            w2=getans(n-2,dp);
+        if(i+2<=n){
+            w2=getans(i+2,n);
         }
 
-        return dp[n]=w1+w2;
+        dp[i]=w1+w2; 
+
+        return w1+w2; 
     }
-    int climbStairs(int n) {
 
-        vector<int> dp(n+1,-1);
-        return getans(n,dp);
+    int climbStairs(int n) {
+        memset(dp,-1,sizeof(dp));    
+        return getans(0,n);
     }
 };
