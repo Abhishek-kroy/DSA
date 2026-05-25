b7b1b634c209b3b354c3212ba473d501987c2d01
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2026-05-25 08:54:51 +0530

    Time: 7 ms (78.74%) | Memory: 27 MB (34.77%) - LeetSync

--- 2001-jump-game-vii/jump-game-vii.cpp
+++ 2001-jump-game-vii/jump-game-vii.cpp
@@ -1,66 +1,47 @@
 class Solution {
 public:
-    bool check(vector<int>& reachable,int l,int r){
-        int left=0;
-        int right=reachable.size()-1;
-
-        while(left<=right){
-            int mid=left+(right-left)/2;
-
-            if(reachable[mid]>=l && reachable[mid]<=r){
-                return true; 
-            }
-            else if(reachable[mid]>r){
-                left=mid+1;
-            }
-            else{
-                right=mid-1; 
-            }
-        }
-
-        return false; 
-    }
-
     bool canReach(string s, int minJump, int maxJump) {
+        int n = s.size();
 
-        int n=s.size();
+        if (s[n - 1] != '0') return false;
 
-        if(s[n-1]!='0'){
-            return false; 
-        }
+        vector<int> vis(n, 0);
+        vis[n - 1] = 1; 
 
-        int i=n-2;
+        int i = n - 2;
+        int ptr1 = n - 1; 
+        int ptr2 = n - 1; 
 
-        vector<int> reachable;
-        reachable.push_back(n-1);
+        while (i >= 0) {
+            if (s[i] == '0') {
+                int l = i + minJump;
+                int r = min(i + maxJump, n - 1); 
 
-        while(i>=0){
-
-            if(s[i]=='0'){
-
-                int l=i+minJump;
-                int r=min(i+maxJump,n-1);
-
-                if(check(reachable,l,r)){
-                    reachable.push_back(i);
+                while (ptr2 > r && ptr2 > ptr1) {
+                    ptr2--;
                 }
 
+                if (r < ptr1 || l > ptr2) {
+                    i--;
+                    continue;
+                }
 
-
-
+                bool found = false;
+                while (ptr2 >= ptr1 && ptr2 >= l) {
+                    if (s[ptr2] == '0' && vis[ptr2]) { 
+                        vis[i] = 1;    
+                        found = true;
+                        ptr1 = i;
+                        break;
+                    }
+                    ptr2--;
+                }
             }
-
-            i--;    
-
-
+            i--;
         }
 
-        int l=reachable.size();
-
-        if(reachable[l-1]!=0){
-            return false;
-        }
-
-        return true; 
+        bool result = (vis[0] == 1);
+        
+        return result;
     }
-};
+}; 
