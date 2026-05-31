06485ae9ad3be0111028655c71fea55d4dfaec40
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2026-06-01 00:16:47 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (18.47%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i=-1;
-        int j=0;
 
         int n=nums.size();
 
-        while(j<n){
+        int i=-1;          
+
+        for(int j=0;j<n;j++){
             if(nums[j]!=0){
-                i++;
-                swap(nums[i],nums[j]);
+                i++;  
+                swap(nums[i],nums[j]);                            
             }
-            j++;
-        }
+        }        
+        
     }
 };
