f5c57b03ba5b68709be5238b4f7de0b535ec5715
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2026-06-03 08:16:53 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (75.59%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -5,8 +5,11 @@ public:
         int m=matrix[0].size(); 
 
         // vector<pair<int,int>> ans; 
-        unordered_set<int> row;
-        unordered_set<int> col; 
+        // unordered_set<int> row;
+        // unordered_set<int> col; 
+
+        bool rzero=false;
+        bool czero=false; 
 
 
         for(int i=0;i<matrix.size();i++){
@@ -17,8 +20,19 @@ public:
                     // p.second=j;
                     // ans.push_back(p);
 
-                    row.insert(i);
-                    col.insert(j);
+                    // row.insert(i);
+                    // col.insert(j);
+
+                    if(i==0){
+                        rzero=true;
+                    }
+
+                    if(j==0){
+                        czero=true; 
+                    }
+
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;  
                 }
             }
         }
@@ -32,16 +46,39 @@ public:
         //     }
         // }
 
-        for(auto r:row){
-            for(int i=0;i<m;i++){
-                matrix[r][i]=0;    
+        for(int j=1;j<n;j++){
+            if(matrix[j][0]!=0){
+                continue; 
+            }
+
+            for(int i=0;i<m;i++){    
+                matrix[j][i]=0;    
             }
         }
 
-        for(auto c:col){
+        for(int j=1;j<m;j++){      
+            if(matrix[0][j]!=0){
+                continue; 
+            }
+
             for(int i=0;i<n;i++){
-                matrix[i][c]=0;    
+                matrix[i][j]=0;    
             }
         }
+
+        if(matrix[0][0]==0){
+
+            if(rzero){
+                for(int j=0;j<m;j++){
+                    matrix[0][j]=0;
+                }
+            }
+
+            if(czero){
+                for(int j=0;j<n;j++){
+                    matrix[j][0]=0;
+                }
+            }
+        }  
     }
 };
