f59d4f8521caa4cfd9d26a904056ab5649b06ef5
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2026-06-04 21:22:22 +0530

    Time: 2 ms (19.32%) | Memory: 27.3 MB (25.78%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n=height.size();
+
+        vector<int> lmax(n,0);
+        vector<int> rmax(n,0);
+
+        int left=0; 
+
+        for(int i=0;i<n;i++){
+            lmax[i]=left;
+
+            left=max(left,height[i]);
+        }
+
+        int right=0; 
+
+        for(int i=n-1;i>=0;i--){
+            rmax[i]=right;
+
+            right=max(right,height[i]);
+        }
+
+        int ans=0;
+
+        for(int i=0;i<n;i++){
+            ans+=max(0,((min(lmax[i],rmax[i])-height[i])));          
+        }
+
+        return ans;    
+        
+    }
+};
