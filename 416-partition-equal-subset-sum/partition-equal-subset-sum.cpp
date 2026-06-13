b1ed69ae37d1a1333ccad68d69413027bbf00068
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2026-06-13 19:52:49 +0530

    Time: 197 ms (11.52%) | Memory: 110.4 MB (38.17%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,36 +1,43 @@
 class Solution {
-    private:
-        bool getans(vector<int>& nums,int sum,vector<vector<int>> &dp,int i){
-            if(sum==0){
-                return true;
-            }
-            if(sum<0 || i>=nums.size()){
-                return false;
-            }
-
-            if(dp[i][sum]!=-1){
-                return dp[i][sum];
-            }
-
-            if(getans(nums,sum-nums[i],dp,i+1))
-                return dp[i][sum]=true;
-            
-            return dp[i][sum]=getans(nums,sum,dp,i+1);
-        }
 public:
-    bool canPartition(vector<int>& nums) {
-        int sum=0;
-        for(auto val:nums){
-            sum+=val;
-        }
+    bool getans(vector<int>& nums,int i,int t,vector<vector<int>>& dp){
+        int n=nums.size();
 
-        if(sum%2){
+        if(t<0){
             return false;
         }
 
-        vector<vector<int>> dp(nums.size(),vector<int>(sum+1,-1));
-        sum/=2;
+        if(i==n){
+            return t==0;
+        }
 
-        return getans(nums,sum,dp,0);
+        if(dp[i][t]!=-1){
+            return dp[i][t];
+        }
+
+        bool w1=getans(nums,i+1,t,dp);
+        bool w2=getans(nums,i+1,t-nums[i],dp);          
+
+        dp[i][t]=w1||w2; 
+
+        return w1||w2;        
+
+    }
+
+    bool canPartition(vector<int>& nums) {
+        int total=0;         
+        for(auto c:nums){
+            total+=c;
+        }
+
+        if(total%2){
+            return false;
+        }
+
+        vector<vector<int>> dp(nums.size(),vector<int>(total/2+1,-1));
+
+        bool ans=getans(nums,0,total/2,dp);   
+        
+        return ans;         
     }
 };
