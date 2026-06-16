af7ed71c54937d34752319ef13882d7c8068b2a0
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2026-06-16 08:01:33 +0530

    Time: 0 ms (100.00%) | Memory: 95 MB (79.43%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,31 +1,43 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int i=0;
-        int j=0;
-        int n=nums1.size();
-        int m=nums2.size();
-        double n1;
-        int n2;
-        int cnt = (n+m)%2==0 ? (n+m)/2 : (n+m)/2+1;
-        nums1.push_back(INT_MAX);
-        nums2.push_back(INT_MAX);
-        // cout<<n<<" "<<m<<" "<<cnt<<endl;
-        while(cnt--){
-            n1=min(nums1[i],nums2[j]);
-            if(nums1[i]<=nums2[j]){
-                i++;
+        int n = nums1.size();
+        int m = nums2.size();
+
+        if (n > m) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int l = 0;
+        int r = n;
+
+        int left = (n + m + 1) / 2;
+
+        while (l <= r) {
+            int cut1 = l + (r - l) / 2;
+            int cut2 = left - cut1;
+
+            int l1 = (cut1 == 0) ? INT_MIN : nums1[cut1 - 1];
+            int r1 = (cut1 == n) ? INT_MAX : nums1[cut1];
+
+            int l2 = (cut2 == 0) ? INT_MIN : nums2[cut2 - 1];
+            int r2 = (cut2 == m) ? INT_MAX : nums2[cut2];
+
+            if (l1 <= r2 && l2 <= r1) {
+                if ((n + m) & 1) {
+                    return max(l1, l2);
+                }        
+
+                return (max(l1, l2) + min(r1, r2)) / 2.0;
             }
-            else{
-                j++;
+            else if (l1 > r2) {
+                r = cut1 - 1;
+            }
+            else {
+                l = cut1 + 1;
             }
         }
-        if((n+m)%2){
-            return n1;
-        }
-        else{
-            n2=min(nums1[i],nums2[j]);
-        }
-        return (n1+n2)/2.0;
+
+        return 0;
     }
-};
+}; 
