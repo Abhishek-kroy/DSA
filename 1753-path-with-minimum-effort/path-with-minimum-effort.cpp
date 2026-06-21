ef47edd345b46d1b3e4b5bd60b69d4890a39fbd3
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2026-06-21 19:53:30 +0530

    Time: 358 ms (5.02%) | Memory: 67.8 MB (6.42%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,39 +1,52 @@
-#define wxy pair<int, pair<int, int>>
 class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
-        int min_effort=INT_MAX;
         int n=heights.size();
         int m=heights[0].size();
-        vector<vector<int>> efforts(n,vector<int> (m,INT_MAX));
-        efforts[0][0]=0;
-        priority_queue<wxy,vector<wxy>,greater<wxy>> pq;
 
-        pq.push({0,{0,0}});
+        vector<vector<int>> vis(n,vector<int> (m,INT_MAX));  
 
-        vector<int> x_dir={-1,0,1,0};
-        vector<int> y_dir={0,-1,0,1};
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
+        pq.push({0,0,0});
+
+        vis[0][0]=0; 
+
+
+        vector<int> dx={-1,0,1,0};
+        vector<int> dy={0,1,0,-1};         
 
         while(!pq.empty()){
-            auto [effort,l] = pq.top();
-            auto [x,y]=l;
+            auto v=pq.top();
+
+            int x=v[0];      
+            int y=v[1];      
+            int val=v[2];                              
 
             pq.pop();
 
-            for(int k=0;k<4;k++){
-                int nx=x+x_dir[k];
-                int ny=y+y_dir[k];
+            if(x==n-1 && y==m-1){
+                return val;
+            }            
+
+            int v1=heights[x][y];        
+
+            for(int i=0;i<4;i++){
+                int nx=x+dx[i];
+                int ny=y+dy[i];
 
                 if(nx>=0 && nx<n && ny>=0 && ny<m){
-                    if(max(effort,abs(heights[nx][ny]-heights[x][y]))<efforts[nx][ny]){
-                        efforts[nx][ny]=max(effort,abs(heights[nx][ny]-heights[x][y]));
-                        pq.push({efforts[nx][ny],{nx,ny}});
+                    int v2=heights[nx][ny];
+
+                    int nval=max(val,abs(v2-v1));
+
+                    if(vis[nx][ny]>nval){
+                        vis[nx][ny]=nval;
+                        pq.push({nx,ny,nval});                                                
                     }
                 }
             }
         }
 
-        return efforts[n-1][m-1];
-
+        return 1;        
     }
 };
