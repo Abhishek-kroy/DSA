fcb5eae8207db74b8349aa60307268d9c4cfe464
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2026-06-21 20:07:34 +0530

    Time: 1088 ms (5.02%) | Memory: 299.7 MB (5.07%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
-    int minimumEffortPath(vector<vector<int>>& heights) {
+    bool check(vector<vector<int>>& heights,int v){
+
+
         int n=heights.size();
         int m=heights[0].size();
 
-        vector<vector<int>> vis(n,vector<int> (m,INT_MAX));  
-
-        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
-        pq.push({0,0,0});
-
-        vis[0][0]=0; 
 
 
-        vector<int> dx={-1,0,1,0};
-        vector<int> dy={0,1,0,-1};         
+        queue<pair<int,int>> q;
+        q.push({0,0});
 
-        while(!pq.empty()){
-            auto v=pq.top();
+        vector<vector<int>> vis(n,vector<int> (m,0));
 
-            int val=v[0];                              
-            int x=v[1];      
-            int y=v[2];              
+        while(!q.empty()){
+            int x=q.front().first;                        
+            int y=q.front().second;
 
-            pq.pop();
+            q.pop();
 
             if(x==n-1 && y==m-1){
-                return val;
-            }            
+                return true;    
+            }
 
-            int v1=heights[x][y];        
+            int v1=heights[x][y];      
+
+            vector<int> dx={-1,0,1,0};                        
+            vector<int> dy={0,1,0,-1};
 
             for(int i=0;i<4;i++){
                 int nx=x+dx[i];
@@ -37,16 +35,33 @@ public:
                 if(nx>=0 && nx<n && ny>=0 && ny<m){
                     int v2=heights[nx][ny];
 
-                    int nval=max(val,abs(v2-v1));
-
-                    if(vis[nx][ny]>nval){
-                        vis[nx][ny]=nval;
-                        pq.push({nval,nx,ny});                                                      
-                    }
+                    if(abs(v2-v1)<=v && !vis[nx][ny]){
+                        vis[nx][ny]=1;
+                        q.push({nx,ny});
+                    }                                        
                 }
+            }                        
+        }    
+
+        return false; 
+    }
+
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int l=0;
+
+        int r=1e6;
+
+        while(l<=r){
+            int mid=l+(r-l)/2;
+
+            if(check(heights,mid)){
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
             }
         }
 
-        return 1;        
+        return l; 
     }
 };
