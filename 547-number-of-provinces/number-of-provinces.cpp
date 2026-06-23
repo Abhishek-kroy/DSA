c6c258b74c5a31d61cf79eca41bdb10cea0ad9e0
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2026-06-23 09:41:28 +0530

    Time: 4 ms (25.47%) | Memory: 19.7 MB (52.41%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,34 +1,67 @@
-class Solution {
-    private:
-    void bfs(int node,vector<vector<int>> & adj,vector<int> &visited){
-        queue<int> q;
-        q.push(node);
-        visited[node]=1;
-        while(!q.empty()){
-            int nod=q.front();
-            q.pop();
-            for(int i=0;i<adj[nod].size();i++){
-                if(adj[nod][i] && !visited[i]){
-                    visited[i]=1;
-                    q.push(i);
-                }
+class dsu{
+    public:
+        vector<int> parent;
+        vector<int> height;
+        int n;
+
+        dsu(int n){
+            parent.resize(n+1);
+            height.resize(n+1);
+
+            for(int i=0;i<=n;i++){
+                parent[i]=i;
+                height[i]=1;
             }
         }
 
-        return ;
-    }
+        int find(int u){
+            if(parent[u]==u){
+                return u;
+            }
+
+            return parent[u]=find(parent[u]); 
+        }
+
+        void unite(int u,int v){
+            int up=find(u);
+            int vp=find(v);
+
+            if(up==vp){
+                return ; 
+            }
+
+            if(height[up]>=height[vp]){
+                parent[vp]=up;
+                height[up]+=height[vp];
+            }
+            else{
+                parent[up]=vp;
+                height[vp]+=height[up];
+            }
+        }
+};
+
+class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int province=0;
-        int vertices=isConnected.size();
-        vector<int> visited(vertices,0);
-        for(int i=0;i<vertices;i++){
-            if(!visited[i]){
-                province++;
-                bfs(i,isConnected,visited);
+
+        int n=isConnected.size();
+
+        dsu* d=new dsu(n);
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j])
+                    d->unite(i+1,j+1);
             }
         }
 
-        return province;
+        unordered_set<int> s;
+
+        for(int i=1;i<=n;i++){
+            s.insert(d->find(i));          
+        }
+
+        return s.size();
     }
 };
