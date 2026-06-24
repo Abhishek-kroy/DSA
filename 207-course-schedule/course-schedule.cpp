58de5fd5c246a5ee2cebc224b6c2ff30df77bdd8
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2026-06-24 08:43:24 +0530

    Time: 7 ms (45.48%) | Memory: 20 MB (38.16%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,34 +1,46 @@
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> indegree(numCourses,0);
-        unordered_map<int,vector<int>> adj;
-        for(int i=0;i<prerequisites.size();i++){
-            indegree[prerequisites[i][0]]++;
-            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+    bool canFinish(int n, vector<vector<int>>& prerequisites) {
+
+        vector<vector<int>> adj(n);
+
+        for(auto p:prerequisites){
+            adj[p[1]].push_back(p[0]);
+        } 
+
+        vector<int> inorder(n);
+
+        for(int i=0;i<n;i++){
+            for(auto nei:adj[i]){
+                inorder[nei]++;
+            }
         }
 
         queue<int> q;
-        for(int i=0;i<numCourses;i++){
-            if(!indegree[i]){
+
+        for(int i=0;i<n;i++){
+            if(!inorder[i]){
                 q.push(i);
             }
         }
 
         int cnt=0;
+
         while(!q.empty()){
             int node=q.front();
             q.pop();
 
-            cnt++;
+            cnt++; 
 
-            for(auto neigh:adj[node]){
-                if(--indegree[neigh]==0){
-                    q.push(neigh);
+            for(auto nei:adj[node]){
+                inorder[nei]--;
+
+                if(!inorder[nei]){
+                    q.push(nei);
                 }
             }
         }
 
-        return cnt==numCourses;
+        return cnt==n; 
     }
 };
