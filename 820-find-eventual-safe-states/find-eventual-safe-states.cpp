43477e79eaf9905ed443260388f727e3beb2eb62
_CODE__KRATOS_ <142518553+Abhishek-kroy@users.noreply.github.com>
2026-06-26 07:22:15 +0530

    Time: 43 ms (57.72%) | Memory: 66.3 MB (19.56%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,49 +1,52 @@
 class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        unordered_map<int,vector<int>> adj;
+        int n=graph.size();
 
-        for(int i=0;i<graph.size();i++){
-            vector<int> neigh=graph[i];
+        vector<vector<int>> adj(n);
 
-            for(auto nei:neigh){
-                adj[nei].push_back(i);
+        for(int i=0;i<n;i++){
+            for(auto v:graph[i]){
+                adj[v].push_back(i);
+            }
+        }        
+
+        vector<int> inorder(n,0);
+
+        for(int i=0;i<n;i++){
+            for(auto nei:adj[i]){        
+                inorder[nei]++;
             }
         }
 
+        queue<int> q;        
 
-        vector<int> indegree(graph.size(),0);
-
-        for(int i=0;i<graph.size();i++){
-            for(auto neigh:adj[i]){
-                indegree[neigh]++;
-            }
-        }
-
-        queue<int> q;
-
-        for(int i=0;i<indegree.size();i++){
-            if(!indegree[i]){
+        for(int i=0;i<n;i++){
+            if(!inorder[i]){
                 q.push(i);
             }
         }
 
         vector<int> ans;
+
         while(!q.empty()){
-            int node=q.front();
+            auto node=q.front();
             q.pop();
 
             ans.push_back(node);
 
-            for(auto neigh:adj[node]){
-                if(--indegree[neigh]==0){
-                    q.push(neigh);
+            for(auto nei:adj[node]){
+
+                inorder[nei]--; 
+
+                if(!inorder[nei]){
+                    q.push(nei);
                 }
             }
         }
 
         sort(ans.begin(),ans.end());
 
-        return ans;
+        return ans;        
     }
 };
