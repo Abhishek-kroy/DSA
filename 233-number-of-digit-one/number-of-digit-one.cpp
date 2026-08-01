f8041291cfe9d8a864e073d5550837a3a45b6482
Abhishek Kumar Roy <abhishekroy2342005@gmail.com>
2026-08-01 17:43:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (19.25%) - LeetSync

--- 233-number-of-digit-one/number-of-digit-one.cpp
+++ 233-number-of-digit-one/number-of-digit-one.cpp
@@ -1,25 +1,52 @@
 class Solution {
 public:
-    int getans(int pos,string digit,int tight,int ones,vector<vector<vector<int>>> &dp){
-        if(pos==digit.size())   return ones;
-        if(dp[pos][tight][ones]!=-1){
-            return dp[pos][tight][ones];
-        }
-        int limit = tight ? digit[pos]-'0' : 9;
+    int ans=0; 
+    pair<int,int> getans(string& num,int i,int tight,vector<vector<pair<int,int>>>& dp,vector<vector<int>>& visited){
+        int n=num.size();
 
-        int w=0;
-
-        for(int i=0;i<=limit;i++){
-            bool newtight=tight&&(i==limit);
-            w+=getans(pos+1,digit,newtight,i==1? ones+1:ones,dp);
+        if(i>=n){
+            return {0,1};
         }
 
-        return dp[pos][tight][ones]=w;
+        if(visited[i][tight]!=-1){
+            return dp[i][tight];
+        }
 
+        int limit = tight ? num[i]-'0':9;
+
+        int cnt=0;        
+
+        int totalnum=0;
+
+        for(int v=0;v<=limit;v++){
+            int ntight=tight && (v==limit);
+            auto [cnt1,nums]=getans(num,i+1,ntight,dp,visited);                
+
+            totalnum+=nums;
+
+            if(v==1){
+                cnt+=(nums)+cnt1;
+            }
+            else{
+                cnt+=(cnt1);
+            }
+        }
+
+        visited[i][tight]=1;        
+
+        dp[i][tight]={cnt,totalnum};
+
+        return {cnt,totalnum};    
     }
     int countDigitOne(int n) {
-        string digit=to_string(n);
-        vector<vector<vector<int>>> dp(digit.size(),vector<vector<int>> (2,vector<int> (digit.size()+1,-1)));
-        return getans(0,digit,1,0,dp);
+        string num=to_string(n);
+
+        // getans(num,0,true).first;              
+
+        vector<vector<pair<int,int>>> dp((int)num.size(),vector<pair<int,int>> (2));                
+        vector<vector<int>> visited((int)num.size(),vector<int> (2,-1))            ; 
+
+
+        return getans(num,0,1,dp,visited).first;        
     }
 };
