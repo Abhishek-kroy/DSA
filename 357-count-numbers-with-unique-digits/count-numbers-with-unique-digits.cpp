52c6036010e23768e19638cb0452c51387354e40
Abhishek Kumar Roy <abhishekroy2342005@gmail.com>
2026-08-08 00:34:18 +0530

    Time: 23 ms (9.46%) | Memory: 7.6 MB (96.77%) - LeetSync

--- 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.cpp
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
+    int getans(int n,int i,int mask){
+        if(i==n){
+            return mask!=0;    
+        }
+
+        int w1=mask==0 ? getans(n,i+1,mask):0;      
+
+        for(int j=0;j<=9;j++){   
+            if(!mask && j==0){
+                continue;
+            }       
+            if(!(mask&(1<<j))){
+                int nmask=mask|(1<<j);
+                w1+=getans(n,i+1,nmask);
+            }
+        }
+
+        return w1;
+    }
     int countNumbersWithUniqueDigits(int n) {
         if(n==0){
             return 1;
         }
-        if(n==1){
-            return 10;
-        }
-        int ans=81;
-        for(int i=2;i<n;i++){
-            ans=ans*(9-i+1);
-        }
 
-        return ans+countNumbersWithUniqueDigits(n-1);
+        return getans(n,0,0)+1;          
     }
 };
