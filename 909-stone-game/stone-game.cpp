7b4013ed5741a834e7715565cf84206645c9f6a6
Abhishek Kumar Roy <abhishekroy2342005@gmail.com>
2026-08-16 22:43:29 +0530

    Time: 242 ms (5.20%) | Memory: 69.4 MB (7.35%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
+    unordered_map<int,unordered_map<int,int>> dp;
+    int getans(vector<int>& piles,int i,int j){
+        int n=piles.size();
+
+        if(i>j){
+            return 0;          
+        }        
+
+        if(dp.find(i)!=dp.end() && dp[i].find(j)!=dp[i].end()){
+            return dp[i][j];
+        }
+
+        int w1=piles[i]-getans(piles,i+1,j);        
+        int w2=piles[j]-getans(piles,i,j-1);
+
+        return dp[i][j]=max(w1,w2);        
+    }
+
     bool stoneGame(vector<int>& piles) {
-        return true; 
+        int ans=getans(piles,0,(int)piles.size()-1);
+        return ans;
     }
 };
