c6b68a826779c268547f25354aaeb7c98ef069ba
Abhishek Kumar Roy <abhishekroy2342005@gmail.com>
2026-08-22 19:46:24 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (10.21%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    unordered_map<int,int> m;
-    int getans(vector<int>& cost,int t){
-        if(t>=(int)cost.size()){      
+    vector<int> dp;
+    int getans(vector<int>& cost,int i){
+        int n=cost.size();
+
+        if(i>=n){
             return 0;
         }
 
-        if(m.find(t)!=m.end()){
-            return m[t];
+        if(dp[i]!=-1){
+            return dp[i];
         }
 
-        if(t==-1){
-            int w1=min(getans(cost,t+1),getans(cost,t+2));
-            m[t]=w1;
-            return  w1;
-        }
+        return dp[i]=cost[i]+min(getans(cost,i+1),getans(cost,i+2));
 
-        int w1=cost[t]+min(getans(cost,t+1),getans(cost,t+2));         
-        m[t]=w1;
-
-        return w1; 
     }
-
     int minCostClimbingStairs(vector<int>& cost) {
-        return getans(cost,-1);        
+        int n=cost.size();
+
+        if(n==1){
+            return cost[0];
+        }
+
+        dp.resize(n,-1);        
+        return min(getans(cost,0),getans(cost,1));
     }
 };
