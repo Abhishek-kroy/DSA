2635e3146ca2d407da4348f52d3d52c8a31d9263
Abhishek Kumar Roy <abhishekroy2342005@gmail.com>
2026-08-24 10:32:25 +0530

    Time: 120 ms (20.35%) | Memory: 99.6 MB (31.77%) - LeetSync

--- 2002-stone-game-viii/stone-game-viii.cpp
+++ 2002-stone-game-viii/stone-game-viii.cpp
@@ -1,29 +1,34 @@
-class Solution {
-public:
-    int stoneGameVIII(vector<int>& stones) {
-        int n=stones.size();
-        cout<<stones[0]<<" ";
-        for(int i=1;i<n;i++){
-            stones[i]+=stones[i-1];
-            cout<<stones[i]<<" ";   
+class Solution { 
+public: 
+    vector<int> dp;
+
+    int getans(vector<int>& stones,int i,int pre){ 
+        int n=stones.size(); 
+ 
+        if(i>=n){ 
+            return 0; 
+        } 
+ 
+        if(i==n-1){ 
+            return pre;            
         }
 
-        // so need a-b value to be optimal 
-        // so both want to maximise the a and b as per them 
-        // so ans tell from i to n-1 what is optimal value to take  
-        // if a split i then b would optimally play so ans   
-
-        // int ans=stones[n-1];  
-
-        vector<int> dp(n,0); 
-        dp[n-1]=stones[n-1];
-
-        for(int i=n-2;i>0;i--){
-            // takes i+1 stones 
-            int alice=stones[i];     
-            dp[i]=max(dp[i+1],alice-dp[i+1]);        
+        if(dp[i] != INT_MIN){
+            return dp[i];
         }
+     
+        int ans=max(
+            pre-getans(stones,i+1,pre+stones[i+1]),
+            getans(stones,i+1,pre+stones[i+1])
+        );         
+ 
+        return dp[i]=ans; 
+    } 
 
-        return dp[1];   
-    }
-};
+    int stoneGameVIII(vector<int>& stones) { 
+        int n=stones.size(); 
+        dp.assign(n,INT_MIN);
+                         
+        return getans(stones,1,stones[0]+stones[1]); 
+    } 
+};    
