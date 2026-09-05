162b0d67f5cd18d2c09491aedd32b11ba03335d7
Abhishek Kumar Roy <abhishekroy2342005@gmail.com>
2026-09-05 21:42:51 +0530

    Time: 4 ms (21.15%) | Memory: 77.4 MB (12.81%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -10,63 +10,35 @@
  */
 class Solution {
 public:
-    int len(ListNode* t1){
-        int l=0;
-        ListNode* h=t1;
-        while(h){
-            l++;
-            h=h->next;
-        }
-        return l;
-    }
-
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
 
-        ListNode* t1=l1;
-        ListNode* t2=l2;
+        ListNode dummy(0);
+        ListNode* temp=&dummy;
 
-        int len1=len(t1);
-        int len2=len(t2);
-        if(len1>=len2){
-            t1=l1;
-            t2=l2;
-        }
-        else{
-            t1=l2;
-            t2=l1;
-        }
-        
-        ListNode* dummy=new ListNode();
-        ListNode* newdummy=dummy;
-        int c=0;
-        while(t2){
-            int v1=t1->val;
-            int v2=t2->val;
-            int v=(v1+v2+c);
-            dummy->next=new ListNode(v%10);
-            dummy=dummy->next;
-            c=(v)/10;
-            t1=t1->next;
-            t2=t2->next;
-        }
-        while(t1){
-            int v1=t1->val;
-            int v2=0;
-            int v=(v1+v2+c);
-            dummy->next=new ListNode(v%10);
-            dummy=dummy->next;
-            c=(v)/10;
-            t1=t1->next;
-        }
-        if(c){
-            dummy->next=new ListNode(c);
-        }
-        return newdummy->next;
+        int carry=0;
 
+        ListNode* t1=l1;        
 
-        return NULL;
+        while(l1 || l2){
+            int v1=l1 ? l1->val:0;
+            int v2=l2 ? l2->val:0;
 
-        
-        
+            int sum=v1+v2+carry;
+
+            int num=sum%10;
+            carry=sum/10;
+
+            temp->next=new ListNode(num);
+            temp=temp->next;  
+
+            l1=l1 ? l1->next:l1;
+            l2=l2 ? l2->next:l2;
+        }
+
+        if(carry){
+            temp->next=new ListNode(carry);        
+        }
+
+        return dummy.next;
     }
 };
