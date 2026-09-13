b1d60c249de94b886661564df6f428ceec08a121
Abhishek Kumar Roy <abhishekroy2342005@gmail.com>
2026-09-13 19:57:23 +0530

    Time: 44 ms (99.72%) | Memory: 43.9 MB (79.51%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,77 +9,28 @@
  */
 class Solution {
 public:
-    int gettotal(TreeNode* root,unordered_map<TreeNode*,int>& depth,int dpt){
+    TreeNode* getans(TreeNode* root,TreeNode* p,TreeNode* q){
         if(!root){
-            return 0;
-        }            
-
-        depth[root]=dpt; 
-
-        return 1+gettotal(root->left,depth,dpt+1)+gettotal(root->right,depth,dpt+1);                    
-    }
-
-    void getup(TreeNode* root,TreeNode* par,unordered_map<TreeNode*,vector<TreeNode*>>& up,int LOG,int n){
-        if(!root){
-            return ;
+            return NULL;
         }
 
-        up[root].resize(LOG+1);
-
-        for(int i=0;i<=LOG;i++){
-            if(i-1>=0)
-                up[root][i]=up[up[root][i-1]][i-1];        
-            else
-                up[root][i]=par; 
+        if(root->val==p->val || root->val==q->val){
+            return root;
         }
 
-        getup(root->left,root,up,LOG,n);
-        getup(root->right,root,up,LOG,n);        
-    }
+        TreeNode* l=getans(root->left,p,q);
+        TreeNode* r=getans(root->right,p,q);
 
+        if(l && r){
+            return root;
+        }
+        if(l){
+            return l;        
+        }
+
+        return r;
+    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        unordered_map<TreeNode*,int> depth;        
-
-        int n=gettotal(root,depth,0);      
-
-        unordered_map<TreeNode*,vector<TreeNode*>> up;
-
-        int LOG =log2(n)+1;
-
-        up[root].resize(LOG+1);
-
-        for(int i=0;i<=LOG;i++){
-            up[root][i]=root;
-        }  
-
-
-        getup(root,root,up,LOG,n);   
-
-        if(depth[p]>depth[q]){
-            swap(p,q);
-        }     
-
-        int d=depth[q]-depth[p];    
-
-        for(int i=LOG;i>=0;i--){
-            if(d&(1<<i)){
-                q=up[q][i];
-            }
-        }        
-
-        if(p==q){
-            return p;
-        }
-
-        for(int i=LOG;i>=0;i--){
-            if(up[p][i]!=up[q][i]){
-                p=up[p][i];
-                q=up[q][i];
-            }                                    
-        }
-
-        return up[p][0]; 
-
-        
+        return getans(root,p,q);
     }
 };
